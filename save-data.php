2e7316f352144c75e4a7b67dff52e4541f849e33
Taras1994m <92815911+Taras1994m@users.noreply.github.com>
2022-12-30 23:14:43 +0100

    Create save-data.php

---
+++ save-data.php
@@ -0,0 +1,22 @@
+document.getElementById('form').addEventListener('submit', function(event) {
+  // Prevent the default form submission behavior
+  event.preventDefault();
+
+  // Get the name from the form
+  var name = document.getElementById('name').value;
+
+  // Send an HTTP request to the server-side script
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', '/save-data.php');
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      // Data saved successfully
+      alert('Data saved successfully');
+    } else {
+      // An error occurred
+      alert('An error occurred');
+    }
+  };
+  xhr.send('name=' + encodeURIComponent(name));
+});
