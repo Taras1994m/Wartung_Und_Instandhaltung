d0c4e3d01f747a5e6fe1e41799d79125e45c8408
Taras1994m <92815911+Taras1994m@users.noreply.github.com>
2022-12-30 23:07:36 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,15 +2,11 @@ document.getElementById('login-form').addEventListener('submit', function(event)
   // Prevent the default form submission behavior
   event.preventDefault();
 
-  // Save the ID in local storage
-  var id = document.getElementById('id').value;
-  localStorage.setItem('id', id);
-
   // Check if the accept-terms checkbox is checked
   var acceptTerms = document.getElementById('accept-terms').checked;
   if (acceptTerms) {
     // Redirect the user to the next page
-    window.location = 'https://www.google.de/';
+    window.location = 'www.google.de';
   } else {
     // Display an error message
     alert('Please accept the terms to proceed.');
