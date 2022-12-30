430d7e5a3de558b8d9bf43e116a75c517d0e7017
Taras1994m <92815911+Taras1994m@users.noreply.github.com>
2022-12-30 23:23:49 +0100

    Create save-data.php

---
+++ save-data.php
@@ -0,0 +1,15 @@
+<?php
+
+// Get the name from the POST parameter
+$name = $_POST['name'];
+
+// Get the current date and time
+$date = date('Y-m-d H:i:s');
+
+// Save the data to the CSV file
+$file = fopen('Data.csv', 'a');
+fputcsv($file, [$name, $date]);
+fclose($file);
+
+// Return a success response
+echo 'Success';
